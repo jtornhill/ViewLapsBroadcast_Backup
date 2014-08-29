9864e34e606df91e60e3714fa6e42f08a783a799
jtornhill <jtl@learningwell.se>
2014-08-29 11:24:14 +0200

    backup

---
+++ CanixMix/CanixMix/FrmChannelA.vb
@@ -0,0 +1,6 @@
+﻿Public Class FrmChannelA
+
+    Private Sub FrmChannelA_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Location = New Point(FrmMain.Size.Width, 0)
+    End Sub
+End Class
