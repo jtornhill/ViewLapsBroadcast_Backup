8969df6f736f8519a407e42a863345b82328e6fd
jtornhill <jtl@learningwell.se>
2014-08-29 12:03:51 +0200

    deleted

--- CanixMix/CanixMix/FrmChannelA.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class FrmChannelA
-
-    Private Sub FrmChannelA_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Location = New Point(FrmMain.Size.Width, 0)
-    End Sub
-End Class
--- CanixMix/CanixMix/FrmChannelB.vb
+++
@@ -1,7 +0,0 @@
-﻿Public Class FrmChannelB
-
- 
-    Private Sub FrmChannelB_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Me.Location = New Point(FrmMain.Size.Width, FrmChannelA.Height)
-    End Sub
-End Class
